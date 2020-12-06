dc076758e0cd31d433621311e7d733b11fae9780
staracek <staracek@foundation.sk>
2020-12-06 17:18:50 +0100

    added simple dummy layout

--- app/src/main/java/sk/lubostar/bignerdguide/geoquiz/MainActivity.kt
+++ app/src/main/java/sk/lubostar/bignerdguide/geoquiz/MainActivity.kt
@@ -2,10 +2,23 @@ package sk.lubostar.bignerdguide.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        true_button.setOnClickListener {
+                view: View -> Toast.makeText(this, R.string.correct_toast,
+                Toast.LENGTH_SHORT).show()
+        }
+
+        false_button.setOnClickListener {
+                view: View -> Toast.makeText(this, R.string.correct_toast,
+            Toast.LENGTH_SHORT).show()
+        }
     }
 }
